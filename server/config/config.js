770df0772f72c255c166a15c6ddd6c9a812ffc98
Rahul <rahulsingh2240@gmail.com>
2018-06-16 16:47:58 +0530

    db configuration

---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+const env = process.env.NODE_ENV || 'development';
+
+if(env === 'development' || env === 'test'){
+
+  let config = require('./config.json');
+  let envconfig = config[env];
+
+  Object.keys(envconfig).forEach((key) => {
+    process.env[key] = envconfig[key];
+  });
+}
