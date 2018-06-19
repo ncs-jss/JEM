36c4b4cf5d1786c85e1f1187a6b483e6dfed4a42
Rahul <rahulsingh2240@gmail.com>
2018-06-19 14:02:54 +0530

    cancelling event

--- server/onesignal/cancel.js
+++ server/onesignal/cancel.js
@@ -1,18 +1,27 @@
 var request = require('request');
 
+var deleteNotification = function(data) {
+
+
 var headers = {
     'Authorization': `Basic ${process.env.RESTAPI}`
 };
 
 var options = {
-    url: 'https://onesignal.com/api/v1/notification/YOUR_NOTIFICATION_ID?app_id=YOUR_APP_ID',
+    method: 'DELETE',
+    url: `https://onesignal.com/api/v1/notifications/${data}?app_id=d3d99984-794f-4c25-bedb-5cb810d8ed86`,
     headers: headers
 };
 
+
 function callback(error, response, body) {
     if (!error && response.statusCode == 200) {
-        console.log(body);
+        console.log('sucees' ,body);
     }
 }
 
 request(options, callback);
+
+};
+
+module.exports = {deleteNotification};
--- server/server.js
+++ server/server.js
@@ -20,6 +20,8 @@ app.set('view engine', 'hbs');
 
 // ONESIGNAL
 const {sendNotification} = require('./onesignal/create');
+const {deleteNotification} = require('./onesignal/cancel');
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
@@ -49,6 +51,7 @@ app.post('/', async (req, res) => {
     _id: id
   });
 
+
   var EventDate = new Date(event.date);
 
   var NotiDate = new Date( EventDate.getTime() - 20000 * 60 );
@@ -64,7 +67,7 @@ app.post('/', async (req, res) => {
        event.notification_id.push(result.id);
          var x = event;
          x.save();
-      
+
    });
 
 });
@@ -118,7 +121,19 @@ app.delete('/events/:id', async (req, res) => {
   }
 
   try {
+
+    let event1 = await Event.findById({
+      _id: id
+    });
+    for (var i = 0, len = event1.notification_id.length; i < len; i++) {
+
+      if (event1.notification_id[i]!==null){
+
+      deleteNotification(event1.notification_id[i]);
+      }
+    }
     const event = await Event.findByIdAndRemove(id);
+
 
     if(!event){
       return res.status(404).send();
