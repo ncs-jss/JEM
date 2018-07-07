640ff9c99ea2f090c16719e3be2d2fcf5922339d
Rahul <rahulsingh2240@gmail.com>
2018-07-07 17:10:38 +0530

    update env example

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('x-auth', '*')
-  res.setHeader('Access-Control-Expose-Headers', 'x-auth') 
+  res.setHeader('Access-Control-Expose-Headers', 'x-auth')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, x-auth')
   res.setHeader('Access-Control-Allow-Credentials', true)
